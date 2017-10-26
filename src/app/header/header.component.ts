11ded63eda1a4f477efc406491d9d06f3bda6519
Anderson Leite <leitea@google.com>
2017-10-25 19:43:38 -0700

    Basic integration with API.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  loadedCharacter: {};
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.http.get('https://rails-playground-api.herokuapp.com/api/header')
+      .map(res => res.json())
+      .subscribe(header => {
+        this.loadedCharacter = header.title;
+      });
+  }
+
+}
