0d74b0d4c9080470f96038ffbd9c6849ae1490f6
Anderson Leite <leitea@google.com>
2017-11-21 21:59:18 -0800

    Setting up Travis CI.

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,15 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+        flags: ['--no-sandbox']
+    }
+  }
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
@@ -12,20 +21,20 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './src/test.ts', watched: false }
+      {pattern: './src/test.ts', watched: false}
     ],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
     },
     mime: {
-      'text/x-typescript': ['ts','tsx']
+      'text/x-typescript': ['ts', 'tsx']
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
